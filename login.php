608fb31786d8fa37e33b66749e4d2f557df0d226
atharvagujarathi <atharvagujarathi92@gmail.com>
2022-08-02 17:59:35 +0530

    mini project

---
+++ login.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+
+</head>
+<body>
+
+<style>
+.aa {
+
+    /* margin-left: 530px;
+    margin-top: 150px;
+     */
+
+}
+
+.ab {
+
+/* margin-left: 830px;
+margin-top: -50px; */
+
+
+
+}
+.bhai {
+color: white;
+text-align: center;
+font-size: 200%;
+
+margin-top: 30px;
+
+}
+@media only screen and (max-width: 768px) {
+  /* For mobile phones: */
+  [class*="okay"] {
+    width: 100%;
+  }
+}
+
+/* .okay {
+     width: 100%;
+} */
+
+.bhai h1 {
+    font-size: 200%;
+font-weight: 10px;
+}
+
+</style>
+
+<div class="okay">
+<div class="navigation-content">
+</div>
+
+<div class="bhai">
+<h1>Online Pizza Delivery Management System</h1><br>  
+</div>
+<center>
+<div class="aa">
+<button style="width: 200px; height: 50px; "><a href="logi.php">User Login</a></button>
+</div>
+
+<div class="ab">
+<button style="width: 200px; height: 50px;"><a href="log.php">Admin Login</a></button>
+</div>
+</center>
+</div>
+
+
+</body>
+</html>
